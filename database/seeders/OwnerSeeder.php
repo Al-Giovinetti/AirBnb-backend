177db392278c889ce7dccaec4118fbc580764947
Al-Giovinetti <giovinettialessio@gmail.com>
2023-10-07 13:03:54 +0200

    ownerseeder data changed

--- database/seeders/OwnerSeeder.php
+++ database/seeders/OwnerSeeder.php
@@ -14,18 +14,61 @@ class OwnerSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(Faker $faker): void
-    {
-        $usersId = User::all()->pluck('id');
+    {   
+        $owners=[
+            [
+                'name'=>'Alessio',
+                'surname'=>'Giovinetti',
+                'age'=>28,
+                'image'=>'aaaaaaaaaaaaaa',
+                'bio'=>'aaaaaaaaaaaaa',
+            ],
+            [
+                'name'=>'Giada',
+                'surname'=>'Pellini',
+                'age'=>29,
+                'image'=>'bbbbbbbbbbb',
+                'bio'=>'bbbbbbbbbbbbbbbbio',
+            ],
+            [
+                'name'=>'Dario',
+                'surname'=>'Giannetti',
+                'age'=>25,
+                'image'=>'ccccccccccccc',
+                'bio'=>'cccccccccccccbio',
+            ],
+            [
+                'name'=>'Annarita',
+                'surname'=>'Baldocchi',
+                'age'=>55,
+                'image'=>'dddddddddddd',
+                'bio'=>'dddddddddddbio',
+            ],
+            [
+                'name'=>'Giuliana',
+                'surname'=>'delCiglio',
+                'age'=>70,
+                'image'=>'eeeeeeeeeee',
+                'bio'=>'eeeeeeeeeeeebio',
+            ],
+            [
+                'name'=>'Eugenio',
+                'surname'=>'Baltorini',
+                'age'=>64,
+                'image'=>'ffffffffffffff',
+                'bio'=>'fffffffffffbio',
+            ],
+        ];
 
-        foreach($usersId as $ownerId){
+        foreach($owners as $key => $owner){
             $newOwner = new Owner();
-            $newOwner->id = $ownerId;
-            $newOwner->user_id = $ownerId;
-            $newOwner->name = $faker->firstName();
-            $newOwner->surname = $faker->lastName();
-            $newOwner->age = $faker->numberBetween(25, 80);
-            $newOwner->image = $faker->imageUrl(640, 480, 'person', true);
-            $newOwner->bio = $faker->paragraph();
+            $newOwner->id = $key+1;
+            $newOwner->user_id = $key+1;
+            $newOwner->name = $owner['name'];
+            $newOwner->surname =  $owner['surname'];
+            $newOwner->age = $owner['age'];
+            $newOwner->image = $owner['image'];
+            $newOwner->bio = $owner['bio'];
             $newOwner->save();
         }
         
