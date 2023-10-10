801128f218993285ec54877d2a15e5db60b34c68
Al-Giovinetti <giovinettialessio@gmail.com>
2023-10-10 17:34:49 +0200

    Responsive Dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,11 +4,60 @@
 
 @include('partials.navbar')
 
-<div class="container">
-    <div class="row">
-        <div class="col-12">
-            <h2>Dashboard</h2>
+<div class="container container-md-fluid">
+    <div class="row d-md-flex justify-content-md-evenly">
+        <div class="col-sm-12 col-md-5 profile-box dashboad-box mb-sm-3 mb-md-4 ">
+            <div class="content">
+                <h2>Il tuo profilo</h2>
+                <p>Visiona il tuo profilo, aggiungi o modifica le tue info.</p>
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-5 houses-box dashboad-box mb-sm-3 mb-md-4">
+            <div class="content">
+                <h2>Le tue abitazioni</h2>
+                <p>Vedi le abitazioni da te registrate, aggiungi e modifica dettagli.</p>
+            </div> 
+        </div>
+        <div class="col-sm-12 col-md-5 statistics-box dashboad-box mb-sm-3 mb-md-4">
+            <div class="content">
+                <h2>Statistiche</h2>
+                <p>Analizza tramite grafici il tuo andamento sulla piattaforma.</p>
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-5 mexs-box dashboad-box mb-sm-3 mb-md-4">
+            <div class="content">
+                <h2>Messaggi</h2>
+                <p>Visualizza tutti i messaggi in arrivo sulla mail da te inserita.</p>
+            </div>
         </div>
     </div>
 </div>
 @endsection
+
+<style>
+    .dashboad-box{
+        height: 50vh;
+        border-radius: 15px;
+        position: relative;
+    }
+
+    div.content{
+        position:absolute;
+        left: 50%; top:50%;
+        transform: translate(-50%,-50%);
+    }
+    .profile-box{
+        border: 1px solid black;
+    }
+    .houses-box{
+        border: 1px solid red;
+    }
+
+    .statistics-box{
+        border: 1px solid yellow;
+    }
+
+    .mexs-box{
+        border: 1px solid pink;
+    }
+</style>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,13 +1,16 @@
 <header class="my_header py-2">
     <div class="container">
         <ul class="row text-md-center m-0">
-            <li class="col-sm-12 col-md-4 list-group-item">
+            <li class="col-sm-12 col-md-3 list-group-item">
+                <a href="">Il tuo profilo</a>
+            </li>
+            <li class="col-sm-12 col-md-3 list-group-item">
                 <a href="">Le tue abitazioni</a>
             </li>
-            <li class="col-sm-12 col-md-4 list-group-item">
+            <li class="col-sm-12 col-md-3 list-group-item">
                 <a href="">Statistiche</a>
             </li>
-            <li class="col-sm-12 col-md-4 list-group-item">
+            <li class="col-sm-12 col-md-3 list-group-item">
                 <a href="">Messaggi</a>
             </li>
         </ul>
@@ -17,5 +20,6 @@
 <style lang="scss" scope>
     header.my_header{
         background-color: red;
+        margin-bottom: 1rem;
     }
 </style>
