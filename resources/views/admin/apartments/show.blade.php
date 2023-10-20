59bd521f12f2a0a5fb936d4827d0f88350aed221
Al-Giovinetti <giovinettialessio@gmail.com>
2023-10-20 16:41:38 +0200

    btn for edit and soft delete

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -33,6 +33,12 @@
                         @endforeach
                     </div>
                 </div>
+                <div class="card-footer d-flex justify-content-around">
+                    <a href="" class="btn btn-primary">Modifica Dati</a>
+                    <form action="">
+                        <button type="submit" class="btn btn-primary">Disabilita struttura</button>  
+                    </form>
+                </div>
             </div>
         </div>
     </div>
