a77591e5be77e7b1318021033c44f979b4ab3cd9
Al-Giovinetti <giovinettialessio@gmail.com>
2023-12-15 12:48:05 +0100

    Basic style for messages page

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -2,5 +2,24 @@
 
 @section('content')
 
-{{$userMessages}}
+@foreach ($userMessages as $message)
+    <div class="message">
+        <p>Messaggio inviato da: {{ $message->sender_email}}</p>
+        <p>{{ $message->content}}</p>
+        <span>{{ $message->updated_at}}</span>
+    </div>
+@endforeach
 @endsection
+
+<style>
+    div.message{
+        padding: 1rem 0.5rem;
+        border: 1px solid black;
+        margin: 0.5rem 1rem;
+    }
+
+    span{
+        display: block;
+        text-align: right;
+    }
+</style>
